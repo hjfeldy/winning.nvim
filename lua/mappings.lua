33fe6eb0a71468b22635176dd8c7a4cd45d551c8
hjfeldy <harry.feldman99@gmail.com>
2022-02-20 18:42:02 -0600

    fixed mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,10 @@
 local map = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 -- Recommended default mappings
-map('n', '<Leader>tt', ':lua terminals:create()<CR>', opts)
-map('n', '<Leader>tn', ':lua terminals:nextTerm()<CR>', opts)
-map('n', '<Leader>tp', ':lua terminals:prevTerm()<CR>', opts)
-map('n', '<Leader>te', ':lua terminals:evenWindows()<CR>', opts)
-map('n', '<Leader>td', ':lua terminals:delete(terminals.recent)()<CR>', opts)
-map('n', '<C-t>', ':lua terminals:toggle()<CR>', opts)
-map('t', '<C-t>', '<C-\\><C-n>:lua terminals:toggle()<CR>', opts)
-map('n', '<Leader>tr', ':lua terminals:rename()<CR>', opts)
+map('n', '<Leader>tt', ':NewTerm()<CR>', opts)
+map('n', '<Leader>tn', ':NextTerm()<CR>', opts)
+map('n', '<Leader>tp', ':PrevTerm()<CR>', opts)
+map('n', '<Leader>te', ':EvenTerms()<CR>', opts)
+map('n', '<C-t>', ':ToggleTerm()<CR>', opts)
+map('t', '<C-t>', '<C-\\><C-n>:ToggleTerm()<CR>', opts)
+map('n', '<Leader>tr', ':RenameTerm()<CR>', opts)
