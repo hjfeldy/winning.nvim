06bbfaf1f6b315c0a4918d1c82c2238b25ea17a0
hjfeldy <harry.feldman99@gmail.com>
2022-02-20 18:44:25 -0600

    fixed mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,10 @@
 local map = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 -- Recommended default mappings
-map('n', '<Leader>tt', ':NewTerm()<CR>', opts)
-map('n', '<Leader>tn', ':NextTerm()<CR>', opts)
-map('n', '<Leader>tp', ':PrevTerm()<CR>', opts)
-map('n', '<Leader>te', ':EvenTerms()<CR>', opts)
-map('n', '<C-t>', ':ToggleTerm()<CR>', opts)
-map('t', '<C-t>', '<C-\\><C-n>:ToggleTerm()<CR>', opts)
-map('n', '<Leader>tr', ':RenameTerm()<CR>', opts)
+map('n', '<Leader>tt', ':NewTerm<CR>', opts)
+map('n', '<Leader>tn', ':NextTerm<CR>', opts)
+map('n', '<Leader>tp', ':PrevTerm<CR>', opts)
+map('n', '<Leader>te', ':EvenTerms<CR>', opts)
+map('n', '<C-t>', ':ToggleTerm<CR>', opts)
+map('t', '<C-t>', '<C-\\><C-n>:ToggleTerm<CR>', opts)
+map('n', '<Leader>tr', ':RenameTerm<CR>', opts)
